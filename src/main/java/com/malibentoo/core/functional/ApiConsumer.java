685a801e14d441a356eea4e175181abf06053321
Mihai <dimamihaimail@gmail.com>
2024-05-04 14:36:37 +0300

    remove unused interface

--- src/main/java/com/malibentoo/core/functional/ApiConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.malibentoo.core.functional;
-
-import com.malibentoo.exception.api.ApiException;
-
-@FunctionalInterface
-public interface ApiConsumer<T> {
-    void accept(T t) throws ApiException;
-}
